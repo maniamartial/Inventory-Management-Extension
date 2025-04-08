2e64c64d3a99ed4b6600065e9f691efbf216a8d4
maniamartial <martialmania19@gmail.com>
2025-04-08 22:01:10 +0300

    chore - remove commented code

--- inventory_management_extension/public/js/stock_entry.js
+++ inventory_management_extension/public/js/stock_entry.js
@@ -35,31 +35,3 @@ function split_iems_(frm){
         frappe.msgprint(__('Please set split count greater than 1 for this item.'));
     }
 }
-// // Optional: Add a button to each item row
-// frappe.ui.form.on('Purchase Receipt Item', {
-//     custom_split_no: function(frm, cdt, cdn) {
-//         let row = locals[cdt][cdn];
-//         let has_split = row.custom_split_no > 1;
-//         if (has_split) {
-//             frappe.call({
-//                 method: "inventory_management_extension.inventory_management_extension.utils.split_purchase_receipt_items",
-//                 args: {
-//                     purchase_receipt: frm.doc.name,
-//                 },
-//                 callback: function(r) {
-//                     if (r.message) {
-//                         frm.refresh_field('items');
-//                         frappe.show_alert({
-//                             message: __('Item successfully split'),
-//                             indicator: 'green'
-//                         });
-//                     }
-//                 },
-//                 freeze: true,
-//                 freeze_message: __('Splitting item...')
-//             });
-//         } else {
-//             frappe.msgprint(__('Please set split count greater than 1 for this item.'));
-//         }
-//     }
-// });
